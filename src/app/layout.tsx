0e07b65e9aaaffb4cf993bb717ae1444bdc36e6d
bugholic <masterav2001@gmail.com>
2024-10-18 11:26:00 +0530

    Added fonts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,7 +7,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className="font-arial">
+      <body className="font-somar">
         {children}
       </body>
     </html>
