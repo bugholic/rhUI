0379cb5abeaeaac9ee764e0688bbfd4f42a2c5b5
bugholic <masterav2001@gmail.com>
2024-10-22 10:30:56 +0530

    save before merge

--- src/components/fourDecades.tsx
+++ src/components/fourDecades.tsx
@@ -3,6 +3,8 @@ import previous from "../assets/icons/previous.svg";
 import next from "../assets/icons/next.svg";
 import Image from "next/image";
 import Heading from "./Heading";
+import { Carousel } from "@material-tailwind/react";
+
 const timelineData = [
   {
     year: 1985,
@@ -43,30 +45,36 @@ const fourDecades = () => {
       <div className="flex flex-row overflow-x-scroll">
         {timelineData.map((item, index) => {
           return (
-            <section className="timelineSection" key={index}>
-              <h3 className="timelineHeading md:hidden text-[#154466] text-[38px] font-semibold">
-                {item.year}
-              </h3>
-              <section className="timelineBarSection">
-                <div className="timelinespot"></div>
-                <div className="timelineLine"></div>
+              <section className="timelineSection" key={index}>
+                <h3 className="timelineHeading md:hidden text-[#154466] text-[38px] font-semibold">
+                  {item.year}
+                </h3>
+                <section className="timelineBarSection">
+                  <div className="timelinespot"></div>
+                  <div className="timelineLine"></div>
+                </section>
+                <h3 className="timelineHeading text-[#154466] text-[38px] font-semibold hidden md:block">
+                  {item.year}
+                </h3>
+                <h5 className="timelineInfo text-[#154466] text-[20px] font-semibold tracking-tighter">
+                  {item.infoHeading}
+                </h5>
+                <p className="timelineInfo text-[#154466] text-[16px] font-normal py-5 tracking-tighter">
+                  {item.info}
+                </p>
               </section>
-              <h3 className="timelineHeading text-[#154466] text-[38px] font-semibold hidden md:block">
-                {item.year}
-              </h3>
-              <h5 className="timelineInfo text-[#154466] text-[20px] font-semibold tracking-tighter">
-                {item.infoHeading}
-              </h5>
-              <p className="timelineInfo text-[#154466] text-[16px] font-normal py-5 tracking-tighter">
-                {item.info}
-              </p>
-            </section>
           );
         })}
       </div>
       <div className="timelineSectionBtn flex">
-        <div className="bg-gray-400 w-fit p-3 m-1 text-center"> <Image src={previous} alt="previous" /></div>
-        <div className="bg-[#C2AB80] w-fit p-3 m-1 text-center"> <Image src={next} alt="next" /></div>
+        <div className="bg-gray-400 w-fit p-3 m-1 text-center">
+          {" "}
+          <Image src={previous} alt="previous" />
+        </div>
+        <div className="bg-[#C2AB80] w-fit p-3 m-1 text-center">
+          {" "}
+          <Image src={next} alt="next" />
+        </div>
       </div>
     </section>
   );
