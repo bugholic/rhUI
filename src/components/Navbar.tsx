e5773ce7a6f84f1efe916da4b01049e44a549dac
bugholic <masterav2001@gmail.com>
2024-10-24 23:31:52 +0530

    added closeMenu

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,18 +1,28 @@
+"use client"
+
 import Image from "next/image";
 import logo from "@/assets/images/riyadhHoldingsLogo.svg";
 import menu from "@/assets/icons/Frame 6menu.svg";
+import closeMenu from "@/assets/icons/closeMenu.svg"
+import { useState } from "react";
 
 const Navbar = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const handleMenuClick = () => {
+    setIsMenuOpen((prev) => !prev);
+  };
+
   return (
     <>
-      <nav className="flex justify-between text-white items-center lg:px-14 p-5 lg:w-[1312px]">
+      <nav className="flex justify-between text-white items-center lg:px-14 p-5 lg:w-[1312px] relative">
         <div>
           <Image src={logo} className="w-[31vw] md:w-[15vw]" alt="Riyadh holding" />
         </div>
         <aside>
-          <Image src={menu} className="w-[5vw] cursor-pointer md:hidden" alt="Riyadh holding"/>
+          <Image src={menu} className="w-[5vw] cursor-pointer md:hidden" alt="Riyadh holding" onClick={handleMenuClick} />
         </aside>
-        <aside className="hidden lg:flex"> 
+        <aside className="hidden lg:flex">
           <div className="navbarItems flex gap-[5vw] tracking-tighter text-sm font-normal">
             <div>Home</div>
             <div>About Us</div>
@@ -27,7 +37,22 @@ const Navbar = () => {
             <option value="English">English</option>
           </select>
         </aside>
-      </nav>
+        {
+          isMenuOpen &&
+          <nav className="menu font-light">
+            <aside>
+              <Image src={closeMenu} className="w-[5vw] pt-5 ml-auto cursor-pointer md:hidden" alt="Riyadh holding" onClick={handleMenuClick} />
+            </aside>
+            <div>Home</div>
+            <div>About Us</div>
+            <div>Our Sectors</div>
+            <div>Media Center</div>
+            <div>Join Us</div>
+            <div>Career</div>
+            <div>Contact Us</div>
+          </nav>
+        }
+      </nav >
     </>
   );
 };
