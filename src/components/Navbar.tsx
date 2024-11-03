5410583c604db1adac9e0ad9cb2c7a8f7fbfcaa3
bugholic <masterav2001@gmail.com>
2024-11-03 19:02:41 +0530

    Added hover in nav bar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -6,9 +6,11 @@ import menu from "@/assets/icons/Frame 6menu.svg";
 import closeMenu from "@/assets/icons/closeMenu.svg";
 import { useState } from "react";
 import downArrow from "@/assets/icons/dropDownArrow.svg"
+import dropDown from "./dropDown";
 
 const Navbar = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const [dropDownStatus, setDropDownStatus] = useState(false);
 
   const handleMenuClick = () => {
     setIsMenuOpen((prev) => !prev);
@@ -35,8 +37,21 @@ const Navbar = () => {
         <aside className="hidden lg:flex items-center">
           <div className="navbarItems text-nowrap flex w-full gap-[5vw] tracking-tighter text-sm font-normal">
             <div>Home</div>
-            <div className="flex items-center">
-              <div>About Us </div><div className="pl-2"><Image src={downArrow} width={16} alt="dropDown" /> </div>
+            <div className="dropdown relative inline-block">
+              <div className="flex items-center cursor-pointer" onClick={() => setDropDownStatus(!dropDownStatus)}>
+                <div>About Us</div>
+                <div className="pl-2">
+                  <Image src={downArrow} width={16} alt="dropDown" className={`transition-transform duration-300 ${dropDownStatus ? 'rotate-180' : ''}`} />
+                </div>
+              </div>
+              {dropDownStatus && (
+                <div className="dropdown-content sm:absolute left-0 mt-2 w-48 shadow-lg rounded-md py-2 z-10">
+                  <a href="#" className="block px-4 py-2 hover:bg-gray-100 hover:text-black ">Overview</a>
+                  <a href="#" className="block px-4 py-2 hover:bg-gray-100 hover:text-black">Vision & Mission</a>
+                  <a href="#" className="block px-4 py-2 hover:bg-gray-100 hover:text-black">Leadership</a>
+                  <a href="#" className="block px-4 py-2 hover:bg-gray-100 hover:text-black">Our History</a>
+                </div>
+              )}
             </div>
             <div className="flex items-center">
               <div>Sector</div><div className="pl-2"><Image src={downArrow} width={16} alt="dropDown" /> </div>
@@ -48,18 +63,16 @@ const Navbar = () => {
             <div>Career</div>
             <div>Contact Us</div>
           </div>
-
-          <div className="dropdown relative inline-block pl-20">
+          <div className="dropdown relative inline-block sm:ml-20">
             <div className="dropbtn cursor-pointer bg-transparent p-4 border-0 flex">
               <div className="flex items-center">
-                <div>العربية</div><Image src={downArrow} width={16} alt="dropDown" /> 
+                <div>العربية</div><Image src={downArrow} width={16} alt="dropDown" />
               </div>
             </div>
             <div className="dropdown-content hidden absolute bg-transparent min-w-40 z-10">
               <a href="#">English</a>
             </div>
           </div>
-          <div className="pl-2"><Image src={downArrow} width={16} alt="dropDown" /> </div>
         </aside>
         {isMenuOpen && (
           <nav className="mobile-nav-view text-2xl">
@@ -71,9 +84,22 @@ const Navbar = () => {
                 onClick={handleMenuClick}
               />
             </aside>
-            <div>Home</div>
-            <div className="flex items-center">
-              <div>About Us </div><div className="pl-2"><Image src={downArrow} width={16} alt="dropDown" /> </div>
+            <div className="activeTab">Home</div>
+            <div className="dropdown relative inline-block">
+              <div className="flex items-center cursor-pointer" onClick={() => setDropDownStatus(!dropDownStatus)}>
+                <div>About Us</div>
+                <div className="pl-2">
+                  <Image src={downArrow} width={16} alt="dropDown" className={`transition-transform duration-300 ${dropDownStatus ? 'rotate-180' : ''}`} />
+                </div>
+              </div>
+              {dropDownStatus && (
+                <div className="dropdown-content left-0 mt-2 w-48 shadow-lg rounded-md py-2 z-10">
+                  <a href="#" className="block px-4 py-2 hover:bg-gray-100">Overview</a>
+                  <a href="#" className="block px-4 py-2 hover:bg-gray-100">Vision & Mission</a>
+                  <a href="#" className="block px-4 py-2 hover:bg-gray-100">Leadership</a>
+                  <a href="#" className="block px-4 py-2 hover:bg-gray-100">Our History</a>
+                </div>
+              )}
             </div>
             <div className="flex items-center">
               <div>Sector</div><div className="pl-2"><Image src={downArrow} width={16} alt="dropDown" /> </div>
@@ -87,7 +113,7 @@ const Navbar = () => {
             <div className="dropdown relative inline-block">
               <div className="dropbtn cursor-pointer bg-transparent p-4 border-0 flex">
                 <div className="flex">
-                  <div>العربية </div><Image src={downArrow} width={16} alt="dropDown" /> 
+                  <div>العربية </div><Image src={downArrow} width={16} alt="dropDown" />
                 </div>
               </div>
               <div className="dropdown-content hidden absolute bg-transparent min-w-40 z-10">
